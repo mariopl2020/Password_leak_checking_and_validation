f82cf2446cceee258b33241d0abb6fa09d4036f0
Mario2 <94698816+mariopl2020@users.noreply.github.com>
2022-04-01 11:34:15 +0200

    Added all validators as objects of single class created in main validator. All tested

--- password_validation.py
+++ password_validation.py
@@ -8,10 +8,10 @@ class Validation(ABC):
     """Interface of validator"""
 
     @abstractmethod
-    def __init__(self, password):
+    def __init__(self, text):
         """Initialization of validator"""
 
-        self.password = password
+        self.text = text
 
     @abstractmethod
     def is_valid(self):
@@ -38,6 +38,82 @@ class HasDigitValidation(Validation):
         return is_valid
 
 
+class HasSpecialCharValidation(Validation):
+    """Represents validator what checks if password has any special character"""
+
+    def __init__(self, password):
+        """Initialization of validator"""
+
+        self.password = password
+
+    def is_valid(self):
+        """Checks if password has any special character
+
+        Returns:
+            is_valid (bool): indicates if password is valid or not"""
+
+        output = [not char.isalnum() for char in self.password]
+        is_valid = any(output)
+        return is_valid
+
+
+class HasLowerCaseValidation(Validation):
+    """Represents validator what checks if password has any low case"""
+
+    def __init__(self, password):
+        """Initialization of validator"""
+
+        self.password = password
+
+    def is_valid(self):
+        """Checks if password has any low case
+
+        Returns:
+            is_valid (bool): indicates if password is valid or not"""
+
+        output = [letter.islower() for letter in self.password]
+        is_valid = any(output)
+        return is_valid
+
+
+class HasUpperCaseValidation(Validation):
+    """Represents validator what checks if password has any upper case"""
+
+    def __init__(self, password):
+        """Initialization of validator"""
+
+        self.password = password
+
+    def is_valid(self):
+        """Checks if password has any upper case
+
+        Returns:
+            is_valid (bool): indicates if password is valid or not"""
+
+        output = [letter.isupper() for letter in self.password]
+        is_valid = any(output)
+        return is_valid
+
+
+class IsLongEnoughValidation(Validation):
+    """Represents validator what checks if password is as long as required"""
+
+    def __init__(self, password, password_length: int = 8):
+        """Initialization of validator"""
+
+        self.password = password
+        self.password_length = password_length
+
+    def is_valid(self):
+        """Checks if password is as long as required
+
+        Returns:
+            is_valid (bool): indicates if password is valid or not"""
+
+        is_valid = len(self.password) >= self.password_length
+        return is_valid
+
+
 class PasswordValidation():
     """Represents main password validator what checks if it is validated by all implemented simple validation
     conditions"""
@@ -47,7 +123,11 @@ class PasswordValidation():
 
         self.user_passwords_set = user_passwords_set
         self.validators = [
-            HasDigitValidation
+            HasDigitValidation,
+            HasSpecialCharValidation,
+            HasUpperCaseValidation,
+            HasLowerCaseValidation,
+            IsLongEnoughValidation
         ]
 
     def is_valid(self):
@@ -59,4 +139,3 @@ class PasswordValidation():
                 if not validator.is_valid():
                     password.is_valid = False
                     break
-
--- tests/password_validation.py
+++ tests/password_validation.py
@@ -1,42 +1,145 @@
 from password import Password
 from passwords_set import PasswordsSet
-from password_validation import HasDigitValidation, PasswordValidation
+from password_validation import (
+	HasDigitValidation,
+	PasswordValidation,
+	HasSpecialCharValidation,
+	HasLowerCaseValidation,
+	HasUpperCaseValidation,
+	IsLongEnoughValidation
+	)
 
 
 def test_if_has_digit_is_valid_positive():
 	"""Checks if method returns correct value, when password fulfil the conditions"""
 
-	#GIVEN
+	# GIVEN
 	test_password = "Adrian4"
 	test_has_digit_validator = HasDigitValidation(test_password)
-	#WHEN
-
-	#THEN
-	assert test_has_digit_validator.is_valid() == True
+	# WHEN
+	is_valid = test_has_digit_validator.is_valid()
+	# THEN
+	assert is_valid == True
 
 
 def test_if_has_digit_is_valid_negative():
 	"""Checks if method returns correct value, when password does not fulfil the conditions"""
 
-	#GIVEN
+	# GIVEN
 	test_password = "Adrian"
 	test_has_digit_validator = HasDigitValidation(test_password)
-	#WHEN
-	#THEN
-	assert test_has_digit_validator.is_valid() == False
+	# WHEN
+	is_valid = test_has_digit_validator.is_valid()
+	# THEN
+	assert is_valid == False
+
+
+def test_if_has_special_char_is_valid_positive():
+	"""Checks if method returns correct value, when password fulfil the conditions"""
+
+	# GIVEN
+	test_password = "Adrian4%"
+	test_has_special_char_validator = HasSpecialCharValidation(test_password)
+	# WHEN
+	is_valid = test_has_special_char_validator.is_valid()
+	# THEN
+	assert is_valid == True
+
+
+def test_if_has_special_char_is_valid_negative():
+	"""Checks if method returns correct value, when password does not fulfil the conditions"""
+
+	# GIVEN
+	test_password = "Adrian4"
+	test_has_special_char_validator = HasSpecialCharValidation(test_password)
+	# WHEN
+	is_valid = test_has_special_char_validator.is_valid()
+	# THEN
+	assert is_valid == False
+
+
+def test_if_has_lower_case_is_valid_positive():
+	"""Checks if method returns correct value, when password fulfil the conditions"""
+
+	# GIVEN
+	test_password = "Adrian4"
+	test_has_lower_case_validator = HasLowerCaseValidation(test_password)
+	# WHEN
+	is_valid = test_has_lower_case_validator.is_valid()
+	# THEN
+	assert is_valid == True
+
+
+def test_if_has_lower_case_is_valid_negative():
+	"""Checks if method returns correct value, when password does not fulfil the conditions"""
+
+	# GIVEN
+	test_password = "ADRIAN4"
+	test_has_lower_case_validator = HasLowerCaseValidation(test_password)
+	# WHEN
+	is_valid = test_has_lower_case_validator.is_valid()
+	# THEN
+	assert is_valid == False
+
+
+def test_if_has_upper_case_is_valid_positive():
+	"""Checks if method returns correct value, when password fulfil the conditions"""
+
+	# GIVEN
+	test_password = "Adrian4"
+	test_has_upper_case_validator = HasUpperCaseValidation(test_password)
+	# WHEN
+	is_valid = test_has_upper_case_validator.is_valid()
+	# THEN
+	assert is_valid == True
+
+
+def test_if_has_upper_case_is_valid_negative():
+	"""Checks if method returns correct value, when password fulfil the conditions"""
+
+	# GIVEN
+	test_password = "adrian4"
+	test_has_upper_case_validator = HasUpperCaseValidation(test_password)
+	# WHEN
+	is_valid = test_has_upper_case_validator.is_valid()
+	# THEN
+	assert is_valid == False
+
+
+def test_if_is_long_enough_positive():
+	"""Checks if method returns correct value, when password fulfil the conditions"""
+
+	# GIVEN
+	test_password = "Adrian45"
+	test_is_long_enough_validator = IsLongEnoughValidation(test_password)
+	# WHEN
+	is_valid = test_is_long_enough_validator.is_valid()
+	# THEN
+	assert is_valid == True
+
+
+def test_if_is_long_enough_negative():
+	"""Checks if method returns correct value, when password does not fulfil the conditions"""
+
+	# GIVEN
+	test_password = "Adrian4"
+	test_is_long_enough_validator = IsLongEnoughValidation(test_password)
+	# WHEN
+	is_valid = test_is_long_enough_validator.is_valid()
+	# THEN
+	assert is_valid == False
 
 
 def test_if_password_is_completely_valid():
 	"""Checks if method returns correctly assign values to passwords after complete password validation"""
 
-	#GIVEN
+	# GIVEN
 	test_passwords_set = PasswordsSet()
-	test_passwords_set.passwords_list = [Password("test"), Password("test123"), Password("tes2t")]
+	test_passwords_set.passwords_list = [Password("test"), Password("Test123%"), Password("T?es2t")]
 	test_password_validation = PasswordValidation(test_passwords_set)
-	excepted_results = [False, True, True]
-	#WHEN
+	excepted_results = [False, True, False]
+	# WHEN
 	test_password_validation.is_valid()
-	#THEN
+	# THEN
 	for index in range(len(test_passwords_set.passwords_list)):
 		assert test_password_validation.user_passwords_set.passwords_list[index].is_valid == excepted_results[index]
-
